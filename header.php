ec8a444f64c77beaa9c6475eaa9587032babdd38
AhmedAbdulsalam7 <ahmed.abdulsalam.udp7@gmail.com>
2022-10-24 07:44:19 +0300

    make it better

---
+++ header.php
@@ -0,0 +1,9 @@
+
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+
+?>
